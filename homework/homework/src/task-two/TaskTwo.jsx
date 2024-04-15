06efea488796ec3ba358425779aae5d57fd3b4d1
Maria <mbezdudnaya@list.ru>
2024-04-15 22:07:27 +0500

    TaskTwo

--- homework/homework/src/task-two/TaskTwo.jsx
+++ homework/homework/src/task-two/TaskTwo.jsx
@@ -4,7 +4,7 @@ import RenderCounter from './render-counter/RenderCounter';
 import './TaskTwo.css';
 
 export default function TaskTwo() {
-    const update = useUpdate()
+    const update = useUpdate() // пользовательский хук, позволяет нам изменять компонент TaskTwo без перерендеривания дочерних компонентов
     return (
         <div className="TaskTwo">
             <button onClick={update}>Обновить компонент</button>
@@ -14,22 +14,19 @@ export default function TaskTwo() {
     )
 }
 
-const Root = memo(() => {
-    const [value, setValue] = useState('')
-    const handleChange = useCallback((event) => {
+const Root = memo(() => { // Компонент Высшего Порядка, оборачивается, мемоизируя тем самым результат, если компонент всегда рендерит одно и то же при неменяющихся пропсах
+    const [value, setValue] = useState('') // используется для хранения состояния в функциональном компоненте, текущее значение состояния и функция обновления состояния
+    const handleChange = useCallback((event) => { // позволяет кэшировать определение функции между повторными рендерингами. 
         setValue(event.target.value)
     }, []); // Мемоизируем функцию handleChange
 
-    const memoizedValue = useMemo(() => {
-        return (
-            <form className="form-container">
-                Введенное значение: {value}
-                <RenderCounter />
-                <Input onChange={handleChange} />
-            </form>
-        );
-    }, [value, handleChange]);
-    return memoizedValue;
+    return (
+        <form className="form-container">
+            Введенное значение: {value}
+            <RenderCounter />
+            <Input onChange={handleChange} />
+        </form>
+    );
 });
 
 const Input = memo(({ onChange }) => {
@@ -43,5 +40,5 @@ const Input = memo(({ onChange }) => {
 
 function useUpdate() {
     const [, setCount] = useState(0)
-    return useCallback(() => { setCount(counter => counter + 1) }, []);
+    return useCallback(() => { setCount(counter => counter + 1) }, []); // используем для того, чтобы возвращаемая функция не менялась и не вызывала перерендеры тех компонентов, функции которых зависят от useUpdate.
 };
--- homework/homework/src/task-two/render-counter/RenderCounter.js
+++ homework/homework/src/task-two/render-counter/RenderCounter.js
@@ -2,9 +2,9 @@ import React, {useEffect, useRef} from 'react';
 import './RenderCounter.css'
 
 const RenderCounter = () => {
-    const renderCount = useRef(0);
+    const renderCount = useRef(0); // хук для хранения любых данных между вызовами компонента, возвращает обычный объект со свойством current внутри. В качестве параметра принимает начальное значение хранимого объекта
 
-    useEffect(() => {
+    useEffect(() => { // Используя этот хук, вы говорите React сделать что-то после рендера. React запомнит функцию (то есть «эффект»), которую вы передали и вызовет её после того, как внесёт все изменения в DOM
         renderCount.current = renderCount.current + 1;
     });
 
